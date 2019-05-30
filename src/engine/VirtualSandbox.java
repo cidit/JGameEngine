fd7caf48618f670213a3f619fb99998eb0a27bce
cid <ciditworld@gmail.com>
2019-05-30 17:28:27 -0400

    did

--- src/engine/VirtualSandbox.java
+++ src/engine/VirtualSandbox.java
@@ -1,5 +1,20 @@
 package engine;
 
-public class VirtualSandbox {
+import java.util.ArrayList;
+import java.util.List;
 
+import collision.Hitbox;
+import props.Prop;
+import util.euclidiangeometry.Coordinate;
+import util.euclidiangeometry.Dimention;
+
+public abstract class VirtualSandbox {
+	
+	protected Hitbox boundaries;
+	protected List<Prop> props = new ArrayList<Prop>();
+
+	public VirtualSandbox(Dimention dimentions) {
+		this.boundaries = new Hitbox(new Coordinate(0, 0), dimentions);
+	}
+	
 }
